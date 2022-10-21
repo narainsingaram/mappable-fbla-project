0eb01ed79a71595f13fc7f98b069acd2c9c5ac1a
Narain Singaram <narain.awesome@gmail.com>
2022-10-21 13:52:41 -0400

    added database

--- .history/includes/configs/configurations_20221021134246.php
+++ .history/includes/configs/configurations_20221021134246.php
@@ -4,7 +4,7 @@ ob_start(); //Turns on output buffering
 session_start();
 
 $timezone = date_default_timezone_set("America/New_York");
-$con = mysqli_connect("localhost", "root", "", "mappable");
+$con = mysqli_connect("localhost", "root", "", "fbla-cdandpgrm-competition");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
