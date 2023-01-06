557d634a7e022d7166b3cf7f75e7069187ea055e
Abdul Ahmed <abdulrafay123ww@gmail.com>
2023-01-05 21:15:09 -0500

    asdasd

--- web/user_report.php
+++ web/user_report.php
@@ -251,7 +251,6 @@ $experience_sum_points = $experience_rows["experience_sum"];
       </div>
       </div>
       </div>
-
       <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
       <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
       <div class="flex-auto p-4">
